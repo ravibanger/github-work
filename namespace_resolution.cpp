12cd820354013630cad6c35de0d60308c4bf04e6
Ravishekhar Banger <ravibanger@gmail.com>
2014-03-10 16:37:22 +0530

    temporary code

--- namespace_resolution.cpp
+++ namespace_resolution.cpp
@@ -19,12 +19,7 @@ namespace bolt { namespace cl { namespace gpu {
 		return 0;
 	}
 
-    template <typename TakenPath1, typename TakenPath2>
-	int function ( bolt::cl::execution_path<TakenPath1> &exec_path1, bolt::cl::execution_path<TakenPath2> &exec_path2)
-	{
-		std::cout << "Execution Path OpenCL GPU\n" ;
-		return 0;
-	}
+
 }}}
 
 
@@ -40,12 +35,7 @@ namespace bolt { namespace cl { namespace cpu {
 		return 0;
 	}
 
-    template <typename TakenPath1, typename TakenPath2>
-	int function ( bolt::cl::execution_path<TakenPath1> &exec_path1, bolt::cl::execution_path<TakenPath2> &exec_path2)
-	{
-		std::cout << "Execution Path OpenCL CPU\n" ;
-		return 0;
-	}
+
 }}}
 
 
@@ -60,12 +50,7 @@ namespace bolt { namespace cl { namespace hlm {
 		return 0;
 	}
 
-    template <typename TakenPath1, typename TakenPath2>
-	int function ( bolt::cl::execution_path<TakenPath1> &exec_path1, bolt::cl::execution_path<TakenPath2> &exec_path2)
-	{
-		std::cout << "Execution Path OpenCL HLM\n" ;
-		return 0;
-	}
+
 }}}
 
 
@@ -80,12 +65,7 @@ namespace bolt { namespace cl {namespace serial {
 		return 0;
 	}
 
-    template <typename TakenPath1, typename TakenPath2>
-	int function ( bolt::cl::execution_path<TakenPath1> &exec_path1, bolt::cl::execution_path<TakenPath2> &exec_path2)
-	{
-		std::cout << "Execution Path Serial CPU\n" ;
-		return 0;
-	}
+
 }}}
 
 
@@ -100,20 +80,53 @@ struct path_t: bolt::cl::execution_path<path_t>{};
 		return 0;
 	}
 
-    template <typename TakenPath1, typename TakenPath2>
-	int function ( bolt::cl::execution_path<TakenPath1> &exec_path1, bolt::cl::execution_path<TakenPath2> &exec_path2)
-	{
-		std::cout << "Execution Path TBB\n" ;
-		return 0;
-	}
+
 }}}
 
 namespace bolt { namespace cl {
 
-    template <typename TakenPath>
-	int function ( bolt::cl::execution_path<TakenPath> &exec_path)
+    enum code_path_enums{
+		SERIAL = 0,
+		CL_GPU,
+		CL_CPU,
+		CL_HLM,
+		TBB
+	};
+	struct control {
+		code_path_enums code_path;
+		bolt::cl::execution_path<> exec_path;
+	};
+
+	template<typename DerivedPolicy>
+	const DerivedPolicy &derived_cast(const execution_path<DerivedPolicy> &x)
 	{
-		function(exec_path);
+	  return static_cast<const DerivedPolicy&>(x);
+	}
+
+	template <typename DerivedPolicy>
+	const DerivedPolicy & take_path(bolt::cl::control &exec_path)
+	{
+
+		if(exec_path.code_path == bolt::cl::SERIAL)
+			return bolt::cl::serial::path;
+		if(exec_path.code_path == bolt::cl::CL_GPU)
+			return bolt::cl::cpu::path;
+		if(exec_path.code_path == bolt::cl::CL_CPU)
+			return bolt::cl::gpu::path;
+		if(exec_path.code_path == bolt::cl::CL_HLM)
+			return bolt::cl::hsa::path;
+	}
+
+	int function ( bolt::cl::control &exec_path)
+	{
+
+		//bolt::cl::execution_path<bolt::cl::cpu::path_t> dummy;
+		//selected_path(exec_path);
+		if(exec_path.code_path == bolt::cl::SERIAL)
+		{
+			bolt::cl::serial::path_t path;
+			function(path);
+		}
 		std::cout << "Execution Path Unknown\n" ;
 		return 0;
 	}
@@ -124,22 +137,20 @@ int main()
 {
 
 	{
-		bolt::cl::execution_path<bolt::cl::cpu::path_t> exec_path1;
-    	bolt::cl::execution_path<bolt::cl::gpu::path_t> exec_path;
-    	function(exec_path);
-    	function(exec_path, exec_path1);
+		bolt::cl::control path;
+		path.code_path = bolt::cl::SERIAL;
+
+    	bolt::cl::function(path);
+
     }
     {
-		bolt::cl::execution_path<bolt::cl::cpu::path_t> exec_path;
-    	function(exec_path);
+		//bolt::cl::execution_path<bolt::cl::cpu::path_t> exec_path;
+    	//function(exec_path);
    	}
     {
-		bolt::cl::execution_path<bolt::cl::serial::path_t> exec_path;
-    	function(exec_path);
+		//bolt::cl::execution_path<bolt::cl::serial::path_t> exec_path;
+    	//function(exec_path);
     }
 
 	return 0;
 }
-
-
-
